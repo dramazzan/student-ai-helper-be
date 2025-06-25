21fa35ce34f7bd40b9c2ea6b0e94d1a7dabf77b4
ramazandautbek <ramazandautbek04@gmail.com>
2025-06-25 22:15:57 +0500

    chore(middleware): updated the method of getting a token from a cookie file

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,22 +1,26 @@
 require('dotenv').config();
-
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
 const authMiddleware = async (req, res, next) => {
-    const token = req.header("Authorization")?.replace("Bearer ", "");
+    const token = req.cookies.token; // ✅ получаем из cookie
+
     if (!token) {
-        return res.status(401).json({success: false, message: "Unauthorized"});
+        return res.status(401).json({ success: false, message: "Unauthorized" });
     }
 
     try {
         const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
         req.user = await User.findById(decodedToken.id);
+
+        if (!req.user) {
+            return res.status(404).json({ success: false, message: "User not found" });
+        }
+
         next();
     } catch (err) {
-        return res.status(500).json({success: false, message: "Something went wrong", error: err.message});
+        return res.status(401).json({ success: false, message: "Invalid token", error: err.message });
     }
-
-}
+};
 
 module.exports = authMiddleware;
